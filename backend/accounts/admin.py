a4779ab589f98101f9e898291ee6539a72ecb90e
Fernando Toledo <fernandohtr@gmail.com>
2024-06-29 16:38:26 -0300

    feat: create custom user admin page

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,7 @@
+from accounts.models import CustomUser
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin): ...
