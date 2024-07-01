b668595006b8760c500c55a35c1d3abf1fa1c5ca
Fernando Toledo <fernandohtr@gmail.com>
2024-07-01 18:02:33 -0300

    feat: add route to retrieve user info

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,4 +1,9 @@
-from accounts.views import UserLoginAPIView, UserLogoutAPIView, UserRegistrationAPIView
+from accounts.views import (
+    UserInfoAPIView,
+    UserLoginAPIView,
+    UserLogoutAPIView,
+    UserRegistrationAPIView,
+)
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -7,4 +12,5 @@ urlpatterns = [
     path("login/", UserLoginAPIView.as_view(), name="login-user"),
     path("logout/", UserLogoutAPIView.as_view(), name="logout-user"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("user/", UserInfoAPIView.as_view(), name="user-info"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,7 +4,7 @@ from accounts.serializers import (
     UserRegistrationSerializer,
 )
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -56,3 +56,11 @@ class UserLogoutAPIView(GenericAPIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInfoAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CustomUserSerializer
+
+    def get_object(self):
+        return self.request.user
